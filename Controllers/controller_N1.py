771c0c4d73639c02a66ca0b71d58d226705de074
Collin <45184637+goodviking@users.noreply.github.com>
2019-02-22 18:12:21 -0500

    Added all directional movement functions

--- Controllers/controller_N1.py
+++ Controllers/controller_N1.py
@@ -2,9 +2,11 @@ import logging
 import os
 import datetime
 import math
+import random
+import time
 
 # Setup a timestamped logfile for this run in the logs directory
-timestamp = datetime.datetime.now().strftime('%Y-%m-%d--%H-%M-%S')
+#timestamp = datetime.datetime.now().strftime('%Y-%m-%d--%H-%M-%S')
 #logfilename = 'D:\\Collin\\Documents\\Python\\MESA Cyber Robot\\Repository\\Logs\\robot_logfile_' + timestamp + '.txt'
 #logging.basicConfig(filename=logfilename,level=logging.DEBUG)
 
@@ -19,21 +21,73 @@ def control_robot(robot):
 
     # Setting up movement functions
     def step_north():
-      if robotProperties.CurrentFacingDirection == 3: # if the direction it is facing is south
-          robot.turn_left()
-          robot.turn_left()
-      elif robotProperties.CurrentFacingDirection == 4: # if the direction it is facing is West
-          robot.turn_right()
-      elif robotProperties.CurrentFacingDirection == 2:
-          robot.turn_left()
-      elif robotProperties.CurrentFacingDirection == 1: # if the direction it is facing is North
-          # Do nothing
+        if robotProperties.CurrentFacingDirection == 3: # if the direction it is facing is South
+            robot.turn_left()
+            robot.turn_left()
+        elif robotProperties.CurrentFacingDirection == 4: # if the direction it is facing is West
+            robot.turn_right()
+        elif robotProperties.CurrentFacingDirection == 2: # if the direction it is facing is East
+            robot.turn_left()
+        elif robotProperties.CurrentFacingDirection == 1: # if the direction it is facing is North
+            #Do nothing
             pass
-    robot.step_forward() # Step forward after now facing North
-    robotProperties.currentPosition[1] = robotProperties.currentPosition[1] + 1 # Change the current Y position by 1
+        robot.step_forward() # Step forward after now facing North
+        robotProperties.currentPosition[1] = robotProperties.currentPosition[1] + 1 # Change the current Y position by 1
+
+    def step_south():
+        if robotProperties.CurrentFacingDirection == 1: # if the direction it is facing is North
+            robot.turn_left()
+            robot.turn_left()
+        elif robotProperties.CurrentFacingDirection == 4: # if the direction it is facing is West
+            robot.turn_left()
+        elif robotProperties.CurrentFacingDirection == 2: # if the direction it is facing is East
+            robot.turn_right()
+        elif robotProperties.CurrentFacingDirection == 3: # if the direction it is facing is South
+            #Do nothing
+            pass
+        robot.step_forward() # Step forward after now facing South
+        robotProperties.currentPosition[1] = robotProperties.currentPosition[1] - 1 # Change the current Y position by -1
+
+    def step_west():
+        if robotProperties.CurrentFacingDirection == 1: # if the direction it is facing is North
+            robot.turn_left()
+        elif robotProperties.CurrentFacingDirection == 3: # if the direction it is facing is South
+            robot.turn_right()
+        elif robotProperties.CurrentFacingDirection == 2: # if the direction it is facing is East
+            robot.turn_left()
+            robot.turn_left()
+        elif robotProperties.CurrentFacingDirection == 4: # if the direction it is facing is West
+            #Do nothing
+            pass
+        robot.step_forward() # Step forward after now facing West
+        robotProperties.currentPosition[0] = robotProperties.currentPosition[0] - 1 # Change the current X position by -1
+
+    def step_east():
+        if robotProperties.CurrentFacingDirection == 1: # if the direction it is facing is North
+            robot.turn_right()
+        elif robotProperties.CurrentFacingDirection == 3: # if the direction it is facing is South
+            robot.turn_left()
+        elif robotProperties.CurrentFacingDirection == 4: # if the direction it is facing is West
+            robot.turn_left()
+            robot.turn_left()
+        elif robotProperties.CurrentFacingDirection == 2: # if the direction it is facing is East
+            #Do nothing
+            pass
+        robot.step_forward() # Step forward after now facing East
+        robotProperties.currentPosition[0] = robotProperties.currentPosition[0] + 1 # Change the current X position by 1
 
     # Getting basic information about the current maze
     packets = robot.sense_packets()
     print(packets)
-    step_north()
-    print("Current Pos: " + str(robotProperties.currentPosition))
+    while True:
+        randomDirection = random.randint(1, 4)
+        if randomDirection == 1:
+            step_north()
+        elif randomDirection == 2:
+            step_east()
+        elif randomDirection == 3:
+            step_south()
+        elif randomDirection == 4:
+            step_west()
+        time.sleep(1)
+        print("Current Pos: " + str(robotProperties.currentPosition))
